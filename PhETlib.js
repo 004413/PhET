68e51aa06d185245d2e94b33e05a9fd8c6626ae0
Robert Soto <robsoto@mit.edu>
2014-06-24 16:09:53 -0400

    slider

--- PhETlib.js
+++ PhETlib.js
@@ -1,6 +1,17 @@
-// $('#theBody').svg();
-$(document).ready(function(){
-$('#theBody').text('Hello there.');
-// var svg = $('#theBody').svg('get'); 
-// $('#theBody').circle(100,100,50);
-});
+function slider(canvas, x, y, w, h) { 
+	var body = canvas.rect(x, y, w, h);
+	var handle = canvas.rect(x, y, (w/10), h);
+	handle.attr({fill:'gray'});
+	body.node.setAttribute('class', 'slider');
+	
+	handle.drag(function(dx,dy,mx,my) { 
+		var bodyX = body.attr('x');
+		var handleX = this.attr('x');
+		var handleW = this.attr('width');
+		var newX = Math.min(bodyX + w - handleW, mx);
+		var newX = Math.max(bodyX, newX);
+		this.attr({x:newX});
+	});
+	
+	
+}
