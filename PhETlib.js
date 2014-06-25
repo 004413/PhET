72044ab0f382c33f7c3c513e1d7eb29fc9eb4777
Robert Soto <robsoto@mit.edu>
2014-06-25 11:57:35 -0400

    added slider value display

--- PhETlib.js
+++ PhETlib.js
@@ -5,30 +5,31 @@ function slider(canvas, x, y, w, h, minVal, maxVal) {
 	var bodyX = body.attr('x');
 	var handleX = handle.attr('x');
 	var handleW = handle.attr('width');
-	this.val = minVal;
 	var self = this;
-	//this.getVal = getSliderVal;
-	//this.initVal = minVal;
+	self.val = parseFloat(minVal).toFixed(3);
+	var disp = canvas.text((x + (w/2)), (y - 10), self.val);
 	handle.attr({fill:'gray'});
 	body.node.setAttribute('class', 'slider');
 	
 	//move slider bar with mouse
-	handle.drag(function(dx,dy,mx,my) { 
+	handle.drag(function(dx,dy,mx,my) { //on move
 		var newX = Math.min(bodyX + w - handleW, mx);
 		var newX = Math.max(bodyX, newX);
 		this.attr({x:newX}) 
 	}, 
-			function() {}, //
-			function() {
+			function() {}, //on start
+			function() { //on end
 				var handleX = handle.attr('x');
 				var bodyX = body.attr('x');
 				var barDist = (handleX + (handleW / 2)) - bodyX;
-				self.val = (barDist / w) * (maxVal - minVal) + minVal; //value based on position of slider bar
+				var value = (barDist / w) * (maxVal - minVal) + minVal;
+				self.val = parseFloat(value).toFixed(3) //value based on position of slider bar
 				if (handleX == bodyX) {
-					self.val = minVal;	
+					self.val = parseFloat(minVal).toFixed(3);	
 				}
 				else if ((handleX + handleW) == (bodyX + w)) {
-					self.val = maxVal;	
+					self.val = parseFloat(maxVal).toFixed(3);	
 				}
+				disp.attr('text', self.val);
 			});
 }
