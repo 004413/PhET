86d9fc118be3c1d063bba69e51664839f58f6c79
Ray Hua Wu <d684n@mit.edu>
2014-06-25 14:55:45 -0400

    placed line-drawing in function

--- linegraph/PhETlib_linegraph.js
+++ linegraph/PhETlib_linegraph.js
@@ -10,6 +10,10 @@ var canvas = Raphael(0,0,R_WIDTH,R_HEIGHT);
 var X_AXIS = canvas.path("M0 "+R_HEIGHT/2+"L"+R_WIDTH+" "+R_HEIGHT/2);
 var Y_AXIS = canvas.path("M"+R_WIDTH/2+" 0L"+R_WIDTH/2+" "+R_HEIGHT);
 
+var GRAPH_SLIDER_MARGIN = 20;
+var MARGIN = 20;
+var SLOPE_MAX = 60;
+
 /* Takes a point in (x,y) coordinates and converts to Raphael canvas coordinates */
 function pointToPathNotation(point){
   var x = point[0];
@@ -19,44 +23,47 @@ function pointToPathNotation(point){
   return [newX,newY]
 }
 
-var coeffs = [0.6,2.3]; // coefficients of line (y=ax+b)
-var a = coeffs[0]; // slope
-var b = coeffs[1]; // y-intercept
-if(a==0){ // case of a horizontal line
-  var point1 = [X_MIN,b];
-  var point2 = [X_MAX,b];
-} else { // case of a non-horizontal line
-  var y_when_x_is_MAX = a*X_MAX+b;
-  var y_when_x_is_MIN = a*X_MIN+b;
-  var x_when_y_is_MAX = (Y_MAX-b)/a;
-  var x_when_y_is_MIN = (Y_MIN-b)/a;
-  /* Corner cases in the following are associated with the edge in the clockwise direction */
-  var intersectsLeft = ((Y_MIN <= y_when_x_is_MIN)&&(y_when_x_is_MIN < Y_MAX))
-  var intersectsBottom = ((X_MIN < x_when_y_is_MIN)&&(x_when_y_is_MIN <= X_MAX))
-  var intersectsRight = ((Y_MIN < y_when_x_is_MAX)&&(y_when_x_is_MAX <= Y_MAX))
-  var intersectsTop = ((X_MIN <= x_when_y_is_MAX)&&(x_when_y_is_MAX < X_MAX))
-  /* Get coordinates of where line intersects the edge of the coordinate grid, to get two points by which to define a line */
-  if(intersectsLeft){
-    var point1 = [X_MIN , y_when_x_is_MIN];
-    if(intersectsBottom){
-      var point2 = [x_when_y_is_MIN , Y_MIN];
-    }else if(intersectsRight){
-      var point2 = [X_MAX , y_when_x_is_MAX];
-    }else if(intersectsTop){
-      var point2 = [x_when_y_is_MAX , Y_MAX];
+function drawLine(slope,yintercept){
+  var a = slope; // slope
+  var b = yintercept; // y-intercept
+  if(a==0){ // case of a horizontal line
+    var point1 = [X_MIN,b];
+    var point2 = [X_MAX,b];
+  } else { // case of a non-horizontal line
+    var y_when_x_is_MAX = a*X_MAX+b;
+    var y_when_x_is_MIN = a*X_MIN+b;
+    var x_when_y_is_MAX = (Y_MAX-b)/a;
+    var x_when_y_is_MIN = (Y_MIN-b)/a;
+    /* Corner cases in the following are associated with the edge in the clockwise direction */
+    var intersectsLeft = ((Y_MIN <= y_when_x_is_MIN)&&(y_when_x_is_MIN < Y_MAX))
+    var intersectsBottom = ((X_MIN < x_when_y_is_MIN)&&(x_when_y_is_MIN <= X_MAX))
+    var intersectsRight = ((Y_MIN < y_when_x_is_MAX)&&(y_when_x_is_MAX <= Y_MAX))
+    var intersectsTop = ((X_MIN <= x_when_y_is_MAX)&&(x_when_y_is_MAX < X_MAX))
+    /* Get coordinates of where line intersects the edge of the coordinate grid, to get two points by which to define a line */
+    if(intersectsLeft){
+      var point1 = [X_MIN , y_when_x_is_MIN];
+      if(intersectsBottom){
+        var point2 = [x_when_y_is_MIN , Y_MIN];
+      }else if(intersectsRight){
+        var point2 = [X_MAX , y_when_x_is_MAX];
+      }else if(intersectsTop){
+        var point2 = [x_when_y_is_MAX , Y_MAX];
+      }
+    }else if(intersectsBottom){
+      var point1 = [x_when_y_is_MIN , Y_MIN];
+      if(intersectsRight){
+        var point2 = [X_MAX , y_when_x_is_MAX];
+      }else if(intersectsTop){
+        var point2 = [x_when_y_is_MAX , Y_MAX];
+      }
+    }else{
+      var point1 = [x_when_y_is_MAX , Y_MAX];
+      var point2 = [X_MAX , y_when_x_is_max];
     }
-  }else if(intersectsBottom){
-    var point1 = [x_when_y_is_MIN , Y_MIN];
-    if(intersectsRight){
-      var point2 = [X_MAX , y_when_x_is_MAX];
-    }else if(intersectsTop){
-      var point2 = [x_when_y_is_MAX , Y_MAX];
-    }
-  }else{
-    var point1 = [x_when_y_is_MAX , Y_MAX];
-    var point2 = [X_MAX , y_when_x_is_max];
   }
+  var pathPoint1 = pointToPathNotation(point1);
+  var pathPoint2 = pointToPathNotation(point2);
+  var plottedLine = canvas.path("M"+pathPoint1[0]+" "+pathPoint1[1]+"L"+pathPoint2[0]+" "+pathPoint2[1]);
 }
-var pathPoint1 = pointToPathNotation(point1);
-var pathPoint2 = pointToPathNotation(point2);
-var plottedLine = canvas.path("M"+pathPoint1[0]+" "+pathPoint1[1]+"L"+pathPoint2[0]+" "+pathPoint2[1]);
+
+drawLine(-3.4,0.3)
