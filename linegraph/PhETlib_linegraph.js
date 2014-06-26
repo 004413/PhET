0ff62c0e58dc5cfb30b15427f5dbb29b75cfd8e3
Ray Hua Wu <d684n@mit.edu>
2014-06-26 10:24:30 -0400

    synced *linegraph.js files

--- linegraph/PhETlib_linegraph.js
+++ linegraph/PhETlib_linegraph.js
@@ -11,7 +11,7 @@ var X_AXIS = lineCanvas.path("M0 "+R_HEIGHT/2+"L"+R_WIDTH+" "+R_HEIGHT/2);
 var Y_AXIS = lineCanvas.path("M"+R_WIDTH/2+" 0L"+R_WIDTH/2+" "+R_HEIGHT);
 var GRAPH_SLIDER_MARGIN = 20;
 var MARGIN = 20;
-var SLOPE_MAX = 60;
+// var SLOPE_MAX = 60;
 
 /* Takes a point in (x,y) coordinates and converts to Raphael canvas coordinates */
 function pointToPathNotation(point){
@@ -27,8 +27,8 @@ function drawLine(slope,yintercept){
   lineCanvas = Raphael(0,0,R_WIDTH,R_HEIGHT);
   var X_AXIS = lineCanvas.path("M0 "+R_HEIGHT/2+"L"+R_WIDTH+" "+R_HEIGHT/2);
   var Y_AXIS = lineCanvas.path("M"+R_WIDTH/2+" 0L"+R_WIDTH/2+" "+R_HEIGHT);
-  var a = slope; // slope
-  var b = yintercept; // y-intercept
+  var a = parseFloat(slope); // slope
+  var b = parseFloat(yintercept); // y-intercept
   if(a==0){ // case of a horizontal line
     var point1 = [X_MIN,b];
     var point2 = [X_MAX,b];
@@ -61,7 +61,7 @@ function drawLine(slope,yintercept){
       }
     }else{
       var point1 = [x_when_y_is_MAX , Y_MAX];
-      var point2 = [X_MAX , y_when_x_is_max];
+      var point2 = [X_MAX , y_when_x_is_MAX];
     }
   }
   var pathPoint1 = pointToPathNotation(point1);
